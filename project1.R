d0806c59b91f13ddca84b97edef9a0a8c75e9639
Rasmus <s164564@student.dtu.dk>
2020-01-14 12:37:58 +0100

    plots and start on NN in python, saved array as data1

--- project1.R
+++ project1.R
@@ -1,3 +1,5 @@
+library("e1071")
+library(plotly)
 load("armdata.RData")
 
 exp2 <- armdata[2][[1]]
@@ -10,4 +12,5 @@ for (i in 1:10){
   }
 }
 
-
+plot(data[1,1,,][,2],data[1,1,,][,3])
+plot_ly(x=data[4,1,,][,1],y=data[4,1,,][,2],z = data[4,1,,][,3],type = "scatter3d")
---
+++ project1_NN.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 12:23:22 2020
+
+@author: Bruger
+"""
+
+import numpy as np
+
+np.loadtxt("data1.txt")
